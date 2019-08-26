696ce1b5a96ed5739f62f08168aff16fd32e2346
user <user@192.168.1.36>
2019-08-26 22:38:16 +0200

    first push
    Change-Id: I7076f54ed2451fa36ec469cf9f361ab1ab05ca5c

---
+++ src/main/java/com/mydemonstration/exercice/MydemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.mydemonstration.exercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydemonstrationApplication.class, args);
+	}
+
+}
